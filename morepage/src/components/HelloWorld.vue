ac595cc060a28c05d12571c611121600540af6d1
apple <839864600@qq.com>
2019-03-12 11:07:35 +0800

    test

--- morepage/src/components/HelloWorld.vue
+++ morepage/src/components/HelloWorld.vue
@@ -1,15 +1,43 @@
 <template>
-  <text class="message">Now, let's use Vue.js to build your Weex app.</text>
+  <div>
+    <text class="message">{{msg}}}</text>
+    <div class="button" @click="getInitialData"><text>click!!!</text></div>
+  </div>
 </template>
 
 <script>
-
+// const initialparams = require
+require('weex-vue-render')
 export default {
   name: 'HelloWorld',
+  data () {
+    return {
+      msg: ''
+    }
+  },
   methods: {
     getInitialData () {
-
+      let urlParamObj = {}
+      try {
+        urlParamObj = utils.parseUrl(window.location.search.slice(1), '&', "=", {maxKeys: 0})
+        console.log(urlParamObj)
+      } catch (error) {
+        console.log('--------------weex.js parseUrl---------------------')
+        console.log(error)
+        console.log('------------------------------------')
+      }
+      for (let key in urlParamObj) {
+        window.weex.config[key] = encodeURIComponent(urlParamObj[key])
+      }
     }
   }
 }
 </script>
+
+<style scoped>
+  .button {
+    width: 200px;
+    height: 200px;
+    background-color: red;
+  }
+</style>
